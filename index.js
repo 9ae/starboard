e8764098ccf730e1679d51ef59fd0ca862926fdc
alice <alice@valour.me>
2018-03-26 20:25:35 +0200

    fandom page in the making

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
+const db = require('./output/data.js')
 
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => res.render('index', {title: 'Style Guide'}))
-app.get('/dashboard', (req, res) => res.render('dashboard', {title: 'Starboard'}))
+app.get('/dashboard', (req, res) => res.render('dashboard',
+  {
+    title: 'Starboard',
+    cats: db.categories
+  }))
+app.get('/fandom/:domname', (req, res) => {
+  let fandom = db.fandoms[req.params.domname];
+  return res.render('fandom', {
+    title: fandom.name,
+    cats: fandom.subdoms
+  });
+})
 
 app.use(express.static('output'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
---
+++ output/data.js
@@ -0,0 +1,26 @@
+module.exports = {
+  categories: ['anime', 'books', 'comics', 'films', 'games', 'tv shows'],
+  fandoms: {
+    'harry_potter': {
+      'name': 'Harry Potter',
+      'subdoms': {
+        'characters': [
+          'Harry Potter',
+          'Ron Weasley',
+          'Hermione Granger',
+          'Albus Dumbledore',
+          'Sirius Black',
+          'Neville Longbottom',
+          'Luna Lovegood',
+          'James Potter',
+          'Draco Malfoy',
+          'Sirius Black',
+          'Severus Snape'
+        ],
+        'ships': [],
+        'houses': ['gryffindor', 'slytherin', 'hufflepuff', 'ravenclaw']
+      },
+      'threads': []
+    }
+  }
+}
