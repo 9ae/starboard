c9046977036c5f5ece1917fa4c6e2ceba35087b3
alice <alice@valour.me>
2018-03-26 18:15:20 +0200

    fixed header stuff

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => res.render('index'))
-app.get('/dashboard', (req, res) => res.render('dashboard'))
+app.get('/', (req, res) => res.render('index', {title: 'Style Guide'}))
+app.get('/dashboard', (req, res) => res.render('dashboard', {title: 'Starboard'}))
 
 app.use(express.static('output'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
