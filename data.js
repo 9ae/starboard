e326282d5dbccf561f069ca6097f2afbe8019258
alice <alice@valour.me>
2018-03-27 10:55:30 +0200

    made data

--- data.js
+++ data.js
@@ -1,38 +1,60 @@
 module.exports = {
-  categories: ['anime', 'books', 'comics', 'films', 'games', 'tv shows'],
+  categories: ["anime", "books", "comics", "films", "games", "tv shows"],
   fandoms: {
-    'harry_potter': {
-      'name': 'Harry Potter',
-      'subdoms': {
-        'characters': [
-          'Harry Potter',
-          'Ron Weasley',
-          'Hermione Granger',
-          'Albus Dumbledore',
-          'Sirius Black',
-          'Neville Longbottom',
-          'Luna Lovegood',
-          'James Potter',
-          'Draco Malfoy',
-          'Sirius Black',
-          'Severus Snape'
+    "harry_potter": {
+      "name": "Harry Potter",
+      "subdoms": {
+        "characters": [
+          "Harry Potter",
+          "Ron Weasley",
+          "Hermione Granger",
+          "Albus Dumbledore",
+          "Sirius Black",
+          "Neville Longbottom",
+          "Luna Lovegood",
+          "James Potter",
+          "Draco Malfoy",
+          "Sirius Black",
+          "Severus Snape"
         ],
-        'ships': [],
-        'houses': ['gryffindor', 'slytherin', 'hufflepuff', 'ravenclaw']
+        "ships": [],
+        "houses": ["gryffindor", "slytherin", "hufflepuff", "ravenclaw"]
       },
-      'threads': [
+      "threads": [
         {
           "type": "fic",
-          "title": "Broken Things",
+          "title": "The Dark Lord's Realm",
           "author": "Anis",
           "source": "ffn",
           "isChaptered": true,
-          "rating": "PG-13",
+          "rating": "R",
           "description": "They disover stuff...",
           "tags": ["slow-burn","post-dh"]
         },
         {
-          
+          "type": "art",
+          "title": "Always said Severus",
+          "author": "Naomi"
+          "thumbnail": "/images/data_potterpuffs.jpg",
+          "description": "Potterpuffs redux",
+          "tags": ["potterpuffs", "snape", "dumbledore"]
+        },
+        {
+          "type": "topic",
+          "title": "Marauders Netflix series?",
+          "replies": 300,
+          "tags": ["speculation"],
+          "author": "cassie_clare"
+        },
+        {
+          "type": "video",
+          "title": "A Very Potter Musical",
+          "author": "Team StarKid",
+          "thumbnail": "data_avpm.png",
+          "url": "https://youtu.be/wmwM_AKeMCk",
+          "source": "youtube",
+          "rating": "PG-13",
+          "tags": ["theatre"]
         }
       ]
     }
